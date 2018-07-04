6612703c64de997d7c879556bfec33b20a7b133e
Alex Pelipenko <vk-tester@yandex.ru>
2018-07-04 18:32:44 +0600

    Add SecondBlock

---
+++ ParserNII/ParserNII/DataStructures/SecondBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace ParserNII.DataStructures
+{
+    public class SecondBlock
+    {
+        public short BitParametrsSecond;
+        public short TurnoversDisel;
+        public short TurnoversTurbochanrger;
+        public short GeneratorPower;
+        public short CurrentGenerator;
+        public short VoltageGenerator;
+        public short Speed;
+        public byte BoostPressure;
+        public short AverageVolumeFuel;
+        public byte FuelDensity;
+        public byte OilDensity;
+        public byte PositionControllerDriver;
+        public byte OilDensityWithFilter;
+    }
+}
